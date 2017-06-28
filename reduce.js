abcf19d31b55bfe465b1c55a5710c478600cba82
Rebecca Tay <rebeccatay92@gmail.com>
2017-06-28 22:25:36 +0800

    completed reduce qns

--- reduce.js
+++ reduce.js
@@ -1,8 +1,9 @@
 var numbers = [1, 56, 2, 4, 1, 99, 3, 5];
 
 // Your code here!
-
+var total = numbers.reduce(function(a,b) {
+  return a * b
+})
+console.log(total)
 // Write something that takes the above array and returns the product of all the numbers
 // via multiplication
-
-
