b733fd2cf2f0a11c9270121efb5b2b2f5d969032
Rebecca Tay <rebeccatay92@gmail.com>
2017-06-28 22:24:33 +0800

    finished map qns

--- map.js
+++ map.js
@@ -1,10 +1,12 @@
 var people = ['Bob', 'Jen', 'James', 'Isolde'];
 
 // Your code here!
-
+var uppercase = people.map(function(indiv) {
+  return indiv.toUpperCase()
+})
+console.log(uppercase)
 // Write some code using map that takes the above array of objects, and turns the array elements to all caps
 
 // ex:
 // console.log(people);
 // > [ 'BOB', 'JEN', 'JAMES', 'ISOLDE' ]
-
